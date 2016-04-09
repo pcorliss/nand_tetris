62bd55c5e5eeef7fe66278d5221dee7cdbbf8b62
Philip Corliss <pcorliss@50projects.com>
2016-04-09 13:41:36 -0500

    Multiplication

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @i
+  M=0
+  @R2
+  M=0
+(LOOP)
+  @i
+  D=M
+  @R0
+  D=M-D // D=R0-i
+  @END
+  D;JLE // If(R0-i <= 0) goto END
+  @R1
+  D=M
+  @R2
+  M=D+M // R2 += R1
+  @i
+  M=M+1 // i++
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
