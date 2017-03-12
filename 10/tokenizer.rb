3e3af849b324f6333d3709f3f2de3f2b3487c051
Philip Corliss <pcorliss@50projects.com>
2017-03-11 21:20:53 -0600

    Working to_xml

--- 10/tokenizer.rb
+++ 10/tokenizer.rb
@@ -53,102 +53,6 @@ IDENTIFIER_REGEX = /^[a-zA-Z_][\w]*$/
 require 'pathname'
 require 'rexml/document'
 
-def tag_name(token)
-  if KEYWORDS.include?(token)
-    'keyword'
-  elsif SYMBOLS.include?(token)
-    'symbol'
-  elsif token.match(IDENTIFIER_REGEX)
-    'identifier'
-  elsif token.match(INTEGER_REGEX) && INTEGER_RANGE.include?(token.to_i)
-    'integerConstant'
-  else
-    'stringConstant'
-  end
-end
-
-def trim_quotes(token)
-  token[1..(token.length-2)]
-end
-
-#path = Pathname.new(ARGV[0])
-
-
-#if path.file?
-  #output_file = ARGV[0].sub(/\.jack$/,'.xml')
-  #files = [ARGV[0]]
-#else
-  #output_file = "#{path}/#{path.basename}.xml"
-  #files = path.children.map(&:to_s).grep(/\.jack$/)
-#end
-
-#File.open(output_file, 'w') do |fh_out|
-  #doc = REXML::Document.new
-  #tokens = doc.add_element('tokens')
-  ##tokens.add_element('keyword').text = ' ' + 'foo' + ' '
-  ##
-  ## getx() -> three params
-  ## // foo \n
-  ## bar -> one token
-  ## /* multiline
-  ## */
-  ##
-  #files.each do |file_name|
-    #fh = File.new(file_name)
-    #comment = false
-    #multiline_comment = false
-    #last_char = ''
-    #working_token = ''
-    #fh.each_char do |char|
-      ##puts "\tChar: #{char} ----- #{working_token}"
-      #last_two = last_char + char
-      #if comment
-        #if last_two == '*/' && multiline_comment
-          #comment = false
-          #multiline_comment = false
-        #end
-        #if char == "\n" && !multiline_comment
-          #comment = false
-        #end
-      #elsif last_two == '//'
-        #comment = true
-        #working_token = ''
-      #elsif last_two == '/*'
-        #multiline_comment = true
-        #comment = true
-        #working_token = ''
-      #elsif char.match(/\s/)
-        #if !working_token.empty?
-          #if tag_name(working_token) != 'stringConstant'
-            #tokens.add_element(tag_name(working_token)).text = working_token
-            ##puts "\t\t#{tag_name(working_token)}::#{working_token}"
-            #working_token = ''
-          #else
-            #working_token << char
-          #end
-        #end
-      #elsif tag_name(char) == 'symbol' && !working_token.empty?
-        #if tag_name(working_token) == 'stringConstant'
-          #working_token = trim_quotes(working_token)
-        #end
-        #tokens.add_element(tag_name(working_token)).text = working_token
-        ##puts "\t\t#{tag_name(working_token)}::#{working_token}"
-        #working_token = ''
-        #tokens.add_element('symbol').text = char
-        ##puts "\t\tsymbol::#{char}"
-      #else
-        #working_token << char
-      #end
-      #last_char = char
-    #end
-  #end
-  #require 'stringio'
-  #s = StringIO.new
-  #doc.write( s, -1)
-  #s.rewind
-  ##puts s.read
-  #puts s.read.gsub(/></,">\n<")
-#end
 
 class Tokenizer
   def initialize(inputfh)
@@ -238,12 +142,58 @@ class Tokenizer
         ['integerConstant', token]
       elsif match = token.match(STRING_REGEX)
         ['stringConstant', match[1]]
+      else
+        ['unknown', token]
       end
     end
+  end
+
+  def init_xml
+    doc = REXML::Document.new
+    doc.add_element('tokens')
+    doc
+  end
+
+  def to_xml(doc = init_xml)
+    tokens = doc.root
+    types.each do |type, token|
+      tokens.add_element(type).text = token
+    end
+    s = StringIO.new
+    doc.write(s , -1)
+    s.rewind
+    s.read
   end
 end
 
 describe Tokenizer do
+  describe "#to_xml" do
+    it "outputs expected xml" do
+      input = <<-EOF
+        // comment
+        let i = 0;
+        // ending comment
+        let j = "this is string";
+        getx(i<43);
+        //
+      EOF
+      s = StringIO.new(input)
+      t = Tokenizer.new(s)
+      t.strip!
+      xml = t.to_xml
+      puts xml
+      expect(xml).to_not include('comment')
+      expect(xml).to_not include('//')
+      expect(xml).to include("<tokens>")
+      expect(xml).to include("</tokens>")
+      expect(xml).to include("<symbol>=</symbol>")
+      expect(xml).to include("<keyword>let</keyword>")
+      expect(xml).to include("<integerConstant>0</integerConstant>")
+      expect(xml).to include("<stringConstant>this is string</stringConstant>")
+      expect(xml).to include("<identifier>i</identifier>")
+    end
+  end
+
   describe "#strip" do
     it "strips single line comments" do
       input = <<-EOF
