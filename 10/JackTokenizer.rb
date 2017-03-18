ab7490d96ac10615fd1b3f07df19e9cba5553e2f
Philip Corliss <pcorliss@50projects.com>
2017-03-18 17:54:01 -0500

    Command line now outputs single files with correct XML

--- 10/JackTokenizer.rb
+++ 10/JackTokenizer.rb
@@ -3,32 +3,34 @@
 require 'pathname'
 require 'rexml/document'
 require_relative 'lib/tokenizer'
+require_relative 'lib/compile_engine'
 
 path = Pathname.new(ARGV[0])
 
 if path.file?
- output_file = ARGV[0].sub(/\.jack$/,'-tokenized.xml')
+ output_file = ARGV[0].sub(/\.jack$/,'Mine.xml')
  files = [ARGV[0]]
 else
- output_file = "#{path}/#{path.basename}-tokenized.xml"
- files = path.children.map(&:to_s).grep(/\.jack$/)
+  raise "Doesn't handle directories yet"
 end
 
 File.open(output_file, 'w') do |fh_out|
-  doc = nil
-
+  #doc = nil
+  xml_str = ''
   files.each do |file_name|
     fh = File.new(file_name)
     t = Tokenizer.new(fh)
     t.strip!
-    doc = t.init_xml
-    t.to_xml(doc)
+    ce =CompileEngine.new(t.types, REXML::Document.new)
+    ce.process!
+
+    xml_str = ce.to_s(strip_whitespace: true, newlines: true, fix_empty: true)
   end
 
   #doc.write(fh_out, -1)
-  s = StringIO.new
-  doc.write( s, -1)
-  s.rewind
+  #s = StringIO.new
+  #doc.write( s, -1)
+  #s.rewind
   #puts s.read
-  fh_out.write s.read.gsub(/></,">\n<")
+  fh_out.write xml_str
 end
