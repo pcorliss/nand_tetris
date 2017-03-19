2ea1606ea47907b4f905ab7668781f221616559e
Philip Corliss <pcorliss@50projects.com>
2017-03-19 15:24:51 -0500

    Project 10 submitted and passed

--- 10/JackAnalyzer.rb
+++ 10/JackAnalyzer.rb
@@ -8,29 +8,21 @@ require_relative 'lib/compile_engine'
 path = Pathname.new(ARGV[0])
 
 if path.file?
- output_file = ARGV[0].sub(/\.jack$/,'Mine.xml')
- files = [ARGV[0]]
+  files = [ARGV[0]]
 else
-  raise "Doesn't handle directories yet"
+  files = Dir.entries(path).grep(/\.jack$/).map { |f| path + f }
 end
 
-File.open(output_file, 'w') do |fh_out|
-  #doc = nil
-  xml_str = ''
-  files.each do |file_name|
-    fh = File.new(file_name)
-    t = Tokenizer.new(fh)
-    t.strip!
-    ce =CompileEngine.new(t.types, REXML::Document.new)
-    ce.process!
+files.each do |file_name|
+  fh = File.new(file_name)
+  t = Tokenizer.new(fh)
+  t.strip!
+  ce =CompileEngine.new(t.types, REXML::Document.new)
+  ce.process!
 
-    xml_str = ce.to_s(strip_whitespace: true, newlines: true, fix_empty: true)
+  xml_str = ce.to_s(strip_whitespace: true, newlines: true, fix_empty: true)
+  output_file = file_name.to_s.sub(/\.jack$/,'.xml')
+  File.open(output_file, 'w') do |fh_out|
+    fh_out.write xml_str
   end
-
-  #doc.write(fh_out, -1)
-  #s = StringIO.new
-  #doc.write( s, -1)
-  #s.rewind
-  #puts s.read
-  fh_out.write xml_str
 end
