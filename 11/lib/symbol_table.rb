4025c3574122330457266896b30ad94f3dba32dc
Philip Corliss <pcorliss@50projects.com>
2017-03-25 18:36:00 -0500

    Simple SymbolTable

---
+++ 11/lib/symbol_table.rb
@@ -0,0 +1,26 @@
+require 'set'
+
+class SymbolTable
+  Symbol = Struct.new(:name, :type, :kind, :offset)
+
+  VALID_KINDS = Set.new(['ARG', 'STATIC', 'FIELD', 'VAR'])
+
+  def initialize
+    @symbols = {}
+    @kind_counts = Hash.new(0)
+  end
+
+  def set(name, type, kind)
+    raise(RuntimeError, "Invalid kind: #{kind}") unless VALID_KINDS.include? kind
+    @symbols[name] = Symbol.new(name, type, kind, @kind_counts[kind])
+    @kind_counts[kind] += 1
+  end
+
+  def get(name)
+    @symbols[name]
+  end
+
+  def exists?(name)
+    @symbols.has_key? name
+  end
+end
---
+++ 11/spec/lib/symbol_table_spec.rb
@@ -0,0 +1,54 @@
+require 'rspec'
+require './lib/symbol_table.rb'
+
+describe SymbolTable do
+  let (:st) { SymbolTable.new }
+
+  describe "get and set" do
+    it "sets a symbol" do
+      st.set('varname', 'int', 'ARG')
+      expect(st.get('varname').name).to eq('varname')
+      expect(st.get('varname').type).to eq('int')
+      expect(st.get('varname').kind).to eq('ARG')
+      expect(st.get('varname').offset).to eq(0)
+    end
+
+    it "increments the offset" do
+      st.set('varname', 'int', 'ARG')
+      st.set('varname2', 'int', 'ARG')
+      st.set('varname3', 'int', 'ARG')
+      expect(st.get('varname').offset).to eq(0)
+      expect(st.get('varname2').offset).to eq(1)
+      expect(st.get('varname3').offset).to eq(2)
+    end
+
+    it "doesn't increment for different kind" do
+      st.set('varname', 'int', 'ARG')
+      st.set('varname2', 'int', 'STATIC')
+      st.set('varname3', 'int', 'FIELD')
+      st.set('varname4', 'int', 'VAR')
+      expect(st.get('varname').offset).to eq(0)
+      expect(st.get('varname2').offset).to eq(0)
+      expect(st.get('varname3').offset).to eq(0)
+      expect(st.get('varname4').offset).to eq(0)
+    end
+
+    it "throws an error if the kind is invalid" do
+      expect do
+        st.set('varname', 'int', 'FOO')
+      end.to raise_error(RuntimeError, "Invalid kind: FOO")
+    end
+  end
+
+  describe "#exists?" do
+    it "returns true if the symbol exists" do
+      st.set('varname', 'int', 'ARG')
+      expect(st.exists?('varname')).to be_truthy
+    end
+
+    it "returns false if the symbol does not exist" do
+      st.set('varname', 'int', 'ARG')
+      expect(st.exists?('fake')).to be_falsey
+    end
+  end
+end
